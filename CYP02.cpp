1057414803f0a8ba31a1b6bd2d51428d4294e2d8
Enrique Mariaca Vazquez <64056718+Acxir8@users.noreply.github.com>
2021-10-06 10:58:08 -0500

    Update CYP02.cpp

--- CYP02.cpp
+++ CYP02.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 int main() {
-
+//quien me copie este codigo se la traga
     int n, m, o=0;
     scanf_s("%i", &n);
     scanf_s("%i", &m);
     printf("BM%08X%08i%08X", n,o, m);
-    return 0;
+       return 0;
 }
