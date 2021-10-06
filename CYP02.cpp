1f50e2b3c6b37d5b9bc9e053bf10098e954e841a
Enrique Mariaca Vazquez <64056718+Acxir8@users.noreply.github.com>
2021-10-06 10:59:29 -0500

    Update CYP02.cpp
    Se agregó un muro protector al código

--- CYP02.cpp
+++ CYP02.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-//quien me copie este codigo se la traga
+//quien me copie este codigo se la tragajeje
     int n, m, o=0;
     scanf_s("%i", &n);
     scanf_s("%i", &m);
