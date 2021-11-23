ab191f3f3d7b2019ce2dbf39da6b62694dd62692
Enrique Mariaca Vazquez <64056718+Acxir8@users.noreply.github.com>
2021-11-23 02:54:04 -0600

    Delete CYP02.cpp

--- CYP02.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-//quien me copie este codigo se la tragajeje
-    int n, m, o=0;
-    scanf_s("%i", &n);
-    scanf_s("%i", &m);
-    printf("BM%08X%08i%08X", n,o, m);
-       return 0;
-}
